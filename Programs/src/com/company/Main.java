174664b96e6741d159c8d82f52f501d34df0acf2
Aditya <adityanaik1302@gmail.com>
2021-08-04 10:19:30 +0530

    added 1st program

---
+++ Programs/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a=10,b=20,sum;
+        sum=a+b;
+        System.out.println("Sum ="+sum);
+    }
+}
