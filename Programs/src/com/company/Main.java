8d70f1645d8a052cb6c3ec8a8e185417899c6072
Aditya <adityanaik1302@gmail.com>
2021-08-05 09:31:48 +0530

    Input added

--- Programs/src/com/company/Main.java
+++ Programs/src/com/company/Main.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a=10,b=20,sum;
-        sum=a+b;
-        System.out.println("Sum ="+sum);
+        int num1,num2,sum;
+        System.out.println("Enter a number");
+        Scanner n1 =new Scanner(System.in);
+        num1=n1.nextInt();
+        System.out.println("Enter the second number");
+        Scanner n2=new Scanner(System.in);
+        num2=n2.nextInt();
+        sum=num1+num2;
+        System.out.println("Sum of two numbers is "+sum);
     }
 }
